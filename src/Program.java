e40089807d42e2cce85d2c929466c2c20278615e
erixian <cibercianocode@gmail.com>
2025-07-22 21:49:59 -0300

    Exemplos sobre interface Set

--- src/Program.java
+++ src/Program.java
@@ -2,13 +2,5 @@ import list.OperacoesBasicas.ListaTarefa;
 
 public class Program {
     public static void main(String[] args) {
-        ListaTarefa listaTarefa = new ListaTarefa();
-        System.out.println(listaTarefa.obterNumeroTotalDeTarefas());
-
-        listaTarefa.adicionarTarefa("Tarefa 1");
-        listaTarefa.adicionarTarefa("Tarefa 2");
-        System.out.println(listaTarefa.obterNumeroTotalDeTarefas());
-
-        listaTarefa.obterDescricaoTarefas();
     }
 }
